eb301d2dd5b38789258e8f12ead23c72db217ee7
livvyvi3 <machinguravongai@gmail.com>
2023-09-22 11:23:25 +0200

    added custom keys

--- app.js
+++ app.js
@@ -1,62 +1,57 @@
-const express = require("express");
 const axios = require("axios");
 require("dotenv").config();
 
-const app = express();
-const PORT = 8080;
+const hubSpotApiKey = process.env.HUBSPOT_KEY;
+const pipedriveApiKey = process.env.PIPE_DRIVE_KEY;
 
-const pipedriveApiKey = process.env.PIPE_DRIVE_KEY; // REPLACE_WITH_YOUR_OWN_KEY
-const hubSpotApiKey = process.env.HUBSPOT_KEY; // REPLACE_WITH_YOUR_HUBSPOT_API_KEY
-const hubSpotContactEndpoint = "https://api.hubapi.com/crm/v3/objects/contacts?limit=10&archived=false";
 
-app.get("/", async (req, res) => {
-  try {
-    const pipedriveResponse = await axios.get(
-      `https://api.pipedrive.com/v1/persons?api_token=${pipedriveApiKey}`
-    );
+axios
+  .get(`https://api.pipedrive.com/v1/persons?api_token=${pipedriveApiKey}`)
+  .then((response) => {
+    const contactData = response.data.data;
+    const customFieldKeyAccMng = 'customFieldKeyAccMng';
+    const cstmFieldAddress = 'cstmFieldAddress';
 
-    if (pipedriveResponse.status === 200) {
-      const persons = pipedriveResponse.data.data;
+  
+    contactData.forEach((contact) => {
+      const email = contact.email[0] ? contact.email[0].value : "";
+      const phone = contact.phone[0] ? contact.phone[0].value : "";
+      const firstname = contact.first_name || '';
+      const lastname = contact.last_name || '';
 
-      const hubSpotContacts = persons.map((person) => {
-        return {
-          properties: {
-            firstname: person.first_name,
-            lastname: person.last_name,
-            email: person.email[0] ? person.email[0].value : "",
-          },
-        };
-      });
+      const country = contact['Add your custome key here'] || '';
+      const account_manager = contact['Add your custome key here'] || '';
+      const address = contact['Add your custome key here'] || '';
+      
+      const fullname = `${firstname} ${lastname}`;
 
-      // Import contacts into HubSpot
-      const hubSpotImportResponse = await axios.post(
-        hubSpotContactEndpoint,
-        hubSpotContacts,
-        {
+      const hubSpotContactData = {
+        properties: {
+          firstname: fullname, 
+          email,
+          phone,
+          country: country,
+          account_manager,
+          address,
+        },
+      };
+      
+      axios
+        .post("https://api.hubapi.com/crm/v3/objects/contacts", hubSpotContactData, {
           headers: {
             accept: "application/json",
             "content-type": "application/json",
             authorization: `Bearer ${hubSpotApiKey}`,
           },
-        }
-      );
-
-      if (hubSpotImportResponse.status === 200) {
-        res.status(200).json({ message: "Contacts imported successfully" });
-      } else {
-        res
-          .status(500)
-          .json({ message: "Error importing contacts into HubSpot" });
-      }
-    } else {
-      res.status(500).json({ message: "Error fetching data from Pipedrive" });
-    }
-  } catch (error) {
-    console.error("Error:", error);
-    res.status(500).json({ message: "Internal server error" });
-  }
-});
-
-app.listen(PORT, () =>
-  console.log(`Server running on port: http://localhost:${PORT}`)
-);
+        })
+        .then((hubSpotResponse) => {
+          console.log("Contact created in HubSpot:", hubSpotResponse.data);
+        })
+        .catch((hubSpotError) => {
+          console.error("Error creating contact in HubSpot:", hubSpotError);
+        });
+    });
+  })
+  .catch((error) => {
+    console.error("Error fetching data from Pipedrive:", error);
+  });
--- index.js
+++ index.js
@@ -9,19 +9,30 @@ axios
   .get(`https://api.pipedrive.com/v1/persons?api_token=${pipedriveApiKey}`)
   .then((response) => {
     const contactData = response.data.data;
+    const customFieldKeyAccMng = '44290adcd518424a6c0be10c17b8f33eb1772264';
+    const cstmFieldAddress = '68cb673c52c6f5143479e1f9d5cf68dcecf0b29e_route';
 
   
     contactData.forEach((contact) => {
       const email = contact.email[0] ? contact.email[0].value : "";
       const phone = contact.phone[0] ? contact.phone[0].value : "";
       const firstname = contact.first_name || '';
+      const lastname = contact.last_name || '';
+
+      const country = contact['68cb673c52c6f5143479e1f9d5cf68dcecf0b29e_country'] || '';
+      const account_manager = contact['44290adcd518424a6c0be10c17b8f33eb1772264'] || '';
+      const address = contact['68cb673c52c6f5143479e1f9d5cf68dcecf0b29e'] || '';
+      
+      const fullname = `${firstname} ${lastname}`;
 
       const hubSpotContactData = {
         properties: {
-          firstname,
+          firstname: fullname, 
           email,
           phone,
-
+          country: country,
+          account_manager,
+          address,
         },
       };
       
